2d86a94bc0581e0eb57b3af83e00bd234f8972ee
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-05-18 21:33:44 +0300

    remove comments

--- src/main.py
+++ src/main.py
@@ -15,32 +15,3 @@ gc=gcody.read(converted)
 vertices=np.array(gc.history)
 
 np.save("pointcloud.npy", vertices)
-
-'''
-
-
-def add_mesh(name, verts, faces, edges=None, col_name="Collection"):    
-    if edges is None:
-        edges = []
-    mesh = bpy.data.meshes.new(name)
-    obj = bpy.data.objects.new(mesh.name, mesh)
-    col = bpy.data.collections[col_name]
-    col.objects.link(obj)
-    bpy.context.view_layer.objects.active = obj
-    mesh.from_pydata(verts, edges, faces)
-
-verts = [( 1.0,  1.0,  0.0), 
-         ( 1.0, -1.0,  0.0),
-         (-1.0, -1.0,  0.0),
-         (-1.0,  1.0,  0.0),
-         ]
-faces = [[0, 1, 2, 3]]
-add_mesh("myBeautifulMesh_1", verts, faces)
-
-verts = [( 3.0,  1.0,  0.0), 
-         ( 3.0, -1.0,  0.0),
-         ( 2.0, -1.0,  0.0),
-         ( 2.0,  1.0,  0.0),
-         ]
-add_mesh("myBeautifulMesh_2", verts, faces)
-'''
