a3b8bf24ec752cae2eb462ea9561d70d9173193a
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-05-24 15:02:11 +0300

    Add printer interface demo

---
+++ src/printer_snap.py
@@ -0,0 +1,25 @@
+from ffpp.Network import Network
+import time
+import asyncio
+import urllib.request
+
+async def main():
+    # sol
+    pr=Network("192.168.0.189")
+    # sag
+    #pr=Network("192.168.0.167")
+
+    await pr.connect()
+    await pr.sendControlRelease()
+
+    try:
+        while True:
+            print(await pr.sendProgressRequest())
+            print(await pr.sendPositionRequest())
+            urllib.request.urlretrieve(f"http://{pr.ip}:8080/?action=snapshot", "camimg.jpg")
+            time.sleep(5)
+    finally:
+        await pr.disconnect()
+
+
+asyncio.run(main())
