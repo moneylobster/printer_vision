f3c16b4673c59ac33dbc309f543cd613bbe54db0
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-05-24 21:15:35 +0300

    stop

--- src/printer_stop.py
+++ src/printer_stop.py
@@ -10,16 +10,8 @@ async def main():
     pr=Network("192.168.0.167")
 
     await pr.connect()
-    await pr.sendControlRelease()
-
-    try:
-        while True:
-            print(await pr.sendProgressRequest())
-            print(await pr.sendPositionRequest())
-            urllib.request.urlretrieve(f"http://{pr.ip}:8080/?action=snapshot", "camimg.jpg")
-            time.sleep(5)
-    finally:
-        await pr.disconnect()
+    await pr.sendAbortRequest()
+    await pr.disconnect()
 
 
 asyncio.run(main())
